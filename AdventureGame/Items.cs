8356d8e349c1cf6142d3dadd8c7faad3ebda220e
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-09-29 16:38:13 +0200

    La till items

---
+++ AdventureGame/Items.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureGame
+{
+    class Items
+    {
+        public string Sunglasses { get; set; }
+        public string Sword { get; set; }
+        public string Shield { get; set; }
+        public string Egg { get; set; }
+        public string Shuriken { get; set; }
+        public string ManaPotion { get; set; }
+        public string HealthPotion { get; set; }
+
+        public Player(string sunglasses, string sword, string shield, string egg, string shuriken, string manapotion, string healthpotion) //: base(10, 10, 10, 10, 10, 10, 10, 10)
+        {
+            Sunglasses = sunglasses;
+            Sword = sword;
+            Shield = shield;
+            Egg = egg;
+            Shuriken = shuriken;
+            ManaPotion = manapotion;
+            HealthPotion = healthpotion;
+        }
+
+    }
+}
