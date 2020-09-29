efc15aaef23aecfa28d1deb1f3d115012c09768c
nidjo pavlovic <e_nidjo@hotmail.com>
2020-09-29 15:50:27 +0200

    Player class

--- AdventureGame/Player.cs
+++ AdventureGame/Player.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AdventureGame
 {
     class Player : Monster
     {
-        
+        public List<string> PlayerBag { get; set; }
 
+        public Player(int hp, int mana, int strength, int endurance, int luck, int agility, int patience, int charisma, List<string> playerBag) :base(hp, mana, strength, endurance, luck, agility, patience, charisma)
+        {
+            PlayerBag = playerBag;
+        }
     }
 }
