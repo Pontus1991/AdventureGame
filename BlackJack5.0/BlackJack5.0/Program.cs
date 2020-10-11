992c3fca09cec3703c85eb9a3bee2bfc71b05689
nidjo pavlovic <e_nidjo@hotmail.com>
2020-10-11 16:01:38 +0200

    Det verkar funka

---
+++ BlackJack5.0/BlackJack5.0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlackJack5._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
---
+++ BlackJack5.0/BlackJack5.0/RunGame.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack5._0
+{
+    class RunGame
+    {
+    }
+}
