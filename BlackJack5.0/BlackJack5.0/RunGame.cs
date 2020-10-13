d626ee4c10fe5c1bf01e8ab5698df6892858955b
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-10-13 14:37:26 +0200

    tja

--- BlackJack5.0/BlackJack5.0/RunGame.cs
+++ BlackJack5.0/BlackJack5.0/RunGame.cs
@@ -14,15 +14,43 @@ namespace BlackJack5._0
         public static Random r = new Random();
         public static List<Card> cardDeck = new List<Card>();
         public static List<Card> summering = new List<Card>();
-        //!!!!Få summering, att fungera, vi vill använda en Linq funktion som summerar det spelet som spelades med hjälp av index i listan.
 
 
-
-        public void Game()
+        public void Meny()
         {
-           
-        }
+            int meny = 0;
+            //Meny för switchen
+            do
+            {
+                Console.WriteLine("Meny för Black Jack");
+                Console.WriteLine("Meny val 1: \nDra ett kort. ");
+                Console.WriteLine("Meny val 2: \nSpelresultat. ");
+                Console.WriteLine("Meny val 3: \nAvsluta spel. ");
+                Int32.TryParse(Console.ReadLine(), out meny);
 
+                switch (meny)
+                {
+
+                    case 1:
+                        DrawCard();//!!!!Skall logiken knytas här eller någon annanstans angående spelarens hand om den är "tjock" eller ej.
+                        break;
+                    case 2:
+                        GameResult(meny);//!!!!Denna metod, skall räkna ut resultaten.
+                        break;
+                    case 3:
+                        EndGame(); //!!!! I anropet, fixa lite stats, finns mer detaljer i metoden.
+                        break;
+                    default:
+                        if (meny > 3)
+                        {
+                            Console.WriteLine("Vänligen ange ett korrekt meny val");
+                        }
+                        break;
+                }
+               
+            } while (meny != 0);
+            
+        }
         public void CreateCardDeck()
         {
             for (int i = 0; i < 4; i++) // Skapa 4 kort.
@@ -51,58 +79,67 @@ namespace BlackJack5._0
                     }
                     else
                     {
-                        Card card = new Card(11, "Äss");
+                        Card card = new Card(11, "Ess");
                         cardDeck.Add(card);
                     }
                 }
             }
         }
         
-        public void Meny()
+        public void GameResult(int menySvar)//Visar spelets "resultat" och kör igång "husets" spel.
         {
-            int meny = 0;
-            //Meny för switchen
-            do
+            
+            int result = summering.Sum(x => x.Value);
+
+            if (result > 21)
             {
-                Console.WriteLine("Meny för Black Jack");
-                Console.WriteLine("Meny val 1: \nDra ett kort. ");
-                Console.WriteLine("Meny val 2: \nSpelresultat. ");
-                Console.WriteLine("Meny val 3: \nAvsluta spel. ");
-                Int32.TryParse(Console.ReadLine(),out meny);
-                
-                switch (meny)
-                {
-                    
-                    case 1: DrawCard();//!!!!Skall logiken knytas här eller någon annanstans angående spelarens hand om den är "tjock" eller ej.
-                        break;
-                    case 2: GameResult();//!!!!Denna metod, skall räkna ut resultaten.
-                        break;
-                    case 3: EndGame(); //!!!! I anropet, fixa lite stats, finns mer detaljer i metoden.
-                        break;
-                    default:
-                        if (meny > 3)
-                        {
-                            Console.WriteLine("Vänligen ange ett korrekt meny val");
-                        }
-                        
-                        break;
-                }
-                Console.WriteLine(RunGame.cardDeck.Count);
-                //Console.Clear(); //!! Denna kanske måste förflyttas ifall inte spelet visas upp korrekt.
-            } while (meny != 0);
-           
+                Console.WriteLine("Du fick " + result + "poäng. Du förlorade.");
+                // Behöver inte calculatebank här eftersom man redan har förlorat. 
+                summering.Clear();
+                Console.ReadLine();
+                Console.Clear(); //!! Denna kanske måste förflyttas ifall inte spelet visas upp korrekt.
+            }
+            else if (menySvar == 2)
+            {
+                Console.WriteLine("Du fick " + result + "poäng. du vann????.");
+                CalculateBank();
+                summering.Clear();
+                Console.ReadLine();
+                Console.Clear(); //!! Denna kanske måste förflyttas ifall inte spelet visas upp korrekt.
+
+            }
         }
-
-        public void GameResult()//Visar spelets "resultat" och kör igång "husets" spel.
+        public void CalculateBank()
         {
+            int result = summering.Sum(x => x.Value);
 
+            while (result < 17) // Banken drar alltid ett kort när den är mindre än 17. Sedan stannaer den.
+            {
+                DrawCard();
+                if (result > 21)
+                {
+                    Console.WriteLine("You won!");
+                    summering.Clear();
+                    Console.ReadLine();
+                    Console.Clear(); //!! Denna kanske måste förflyttas ifall inte spelet visas upp korrekt.
+                    Meny();
+                }
+                else if (result == 21)
+                {
+                    Console.WriteLine("You Lost!");
+                    summering.Clear();
+                    Console.ReadLine();
+                    Console.Clear(); //!! Denna kanske måste förflyttas ifall inte spelet visas upp korrekt.
+                    Meny();
+                }
+
+            }
         }
         public void Shuffler()
         {           
             for (int i = 0; i < cardDeck.Count - 1; i++)
             {
                 int k = r.Next(i + 1);
-
                 int temp = cardDeck[i].Value;
                 cardDeck[i].Value = cardDeck[k].Value;
                 cardDeck[k].Value = temp;
@@ -115,14 +152,49 @@ namespace BlackJack5._0
                 CreateCardDeck();
                 Shuffler();
             }
+
             for (int i = 0; i <= 0; i++)
             {
                 Console.WriteLine($"Du fick {cardDeck[i].Value}");
+                if (cardDeck[i].Value == 11)
+                {
+                    try
+                    {
+                        Console.WriteLine("Tryck 1 för att omvandla esset till en etta! Annars tryck enter! ");
+                        string answer = Console.ReadLine();
+                        if (answer == "1")
+                        {
+                            cardDeck[i].Value = 1;
+                            Console.WriteLine("Hej hej det funkar");
+                        }
+                        else 
+                        {
+                            cardDeck[i].Value = 11;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                    
+                }
                 summering.Add(cardDeck[i]);
+                Console.WriteLine("Handen: " + summering.Sum(x => x.Value));
                 cardDeck.Remove(cardDeck[i]);
             }
-            int result = summering.Sum(x => x.Value);
-            Console.WriteLine(result);
+
+            int Tjock = summering.Sum(x => x.Value);
+
+            if (Tjock > 21)
+            {
+                Console.WriteLine("You lost! ");
+                summering.Clear();
+                Console.ReadLine();
+                Console.Clear(); //!! Denna kanske måste förflyttas ifall inte spelet visas upp korrekt.
+                Meny();
+            }
+            
         }
         public void GameLogic()//Kör igång spelet efter att RunGame har skapats i Main.
         {
