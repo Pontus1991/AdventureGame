3184444397f36edf86f69b40f5d1f55f6b67bd49
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-10-14 23:10:48 +0200

    Fick till en ruta

--- SnakeKing/SnakeKing/WorldMap.cs
+++ SnakeKing/SnakeKing/WorldMap.cs
@@ -8,37 +8,58 @@ namespace SnakeKing
     {
         //Ifall vi inte får detta att fungera i metodanropet i RunGame, skapa bara WorldMap i RunGame. -> Kopiera helst den koden då
         public int WidthX { get; set; }
-        public int LengthY { get; set; }
-        
+        public int HeightY { get; set; }
 
-        public WorldMap(int widthX, int lengthY)
+        public char[,] Matris { get; set; }
+
+
+        public WorldMap(int widthX, int heightY)
         {
             WidthX = widthX;
-            LengthY = lengthY;
+            HeightY = heightY;
             
         }
         public WorldMap()
         {
-            WidthX = 64;
-            LengthY = 44;
-            
+            WidthX = 54;
+            HeightY = 25;
         }
 
 
         public void NewMap()
         {
-            Console.SetCursorPosition(0, 0);
-            Console.Write("╔");
-            Console.SetCursorPosition((WidthX + 1), 0);
-            Console.Write("╗");
-
-            for (int i = 1; i < WidthX; i++)
+            for (int i = 1; i < WidthX + 1; i++) // Yttre forloop går igneom VÅGRÄTT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             {
                 Console.SetCursorPosition(i, 0);
                 Console.Write("═");
             }
-         
 
+            for (int p = 0; p < WidthX + 1; p++)
+            {
+                Console.SetCursorPosition(p, HeightY -1);
+                Console.Write("═");
+            }
+
+            for (int j = 0; j < HeightY -1; j++)
+            {
+                Console.SetCursorPosition(0, j);
+                Console.Write("║");
+            }
+
+            for (int n = 0; n < HeightY -1; n++)
+            {
+                Console.SetCursorPosition(WidthX, n);
+                Console.Write("║");
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write("╔");
+            Console.SetCursorPosition((WidthX), 0);
+            Console.Write("╗");
+            Console.SetCursorPosition(0, HeightY -1);
+            Console.Write("╚");
+            Console.SetCursorPosition((WidthX), HeightY -1);
+            Console.Write("╝");
         }
         
 
