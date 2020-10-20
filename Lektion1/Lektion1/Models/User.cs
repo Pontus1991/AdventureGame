9b243e01f1eaf0f30c3a4fa0f3279b4cc385da43
nidjo pavlovic <e_nidjo@hotmail.com>
2020-10-20 18:39:18 +0200

    TJAAAAAAAAAAAAAAA!!!!!!!!!!!!!!!

---
+++ Lektion1/Lektion1/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lektion1.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+
+        public User()
+        {
+            
+        }
+        public User(string userName, int age)
+        {
+            UserName = userName;
+            Age = age;
+        }
+    }
+}
