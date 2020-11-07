bfba15921a771ea14d7eadb627176f6e22b1b358
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-11-07 22:56:49 +0100

    lagt till view till login scaffold

---
+++ SlutUppgift/SlutUppgift/Areas/Identity/Data/SlutUppgiftContext2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SlutUppgift.Areas.Identity.Data;
+
+namespace SlutUppgift.Data
+{
+    public class SlutUppgiftContext2 : IdentityDbContext<SlutUppgiftUser>
+    {
+        public SlutUppgiftContext2(DbContextOptions<SlutUppgiftContext2> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ SlutUppgift/SlutUppgift/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SlutUppgift.Areas.Identity.Data;
+using SlutUppgift.Data;
+
+[assembly: HostingStartup(typeof(SlutUppgift.Areas.Identity.IdentityHostingStartup))]
+namespace SlutUppgift.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SlutUppgiftContext2>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SlutUppgiftContext2Connection")));
+
+                services.AddDefaultIdentity<SlutUppgiftUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SlutUppgiftContext2>();
+            });
+        }
+    }
+}
