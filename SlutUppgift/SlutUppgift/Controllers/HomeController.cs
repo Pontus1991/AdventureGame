9137cbcf763e468d7db2269a552691300ce9dd7e
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-11-07 13:51:31 +0100

    tjena3

--- SlutUppgift/SlutUppgift/Controllers/HomeController.cs
+++ SlutUppgift/SlutUppgift/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace SlutUppgift.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)] // tja
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
