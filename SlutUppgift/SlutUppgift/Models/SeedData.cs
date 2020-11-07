af63a42a75a46634f23197231e7ac5953c52e7f1
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-11-07 11:32:23 +0100

    tja

---
+++ SlutUppgift/SlutUppgift/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SlutUppgift.Controllers;
+using SlutUppgift.Data;
+using SlutUppgift.Models;
+using System;
+using System.Linq;
+
+namespace SlutUppgift.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SlutUppgiftContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<SlutUppgiftContext>>()))
+            {    
+                if (context.Track.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Track.AddRange(
+                    new Track
+                    {
+                        Price = 5,
+                        Age = 25,
+                        FirstName = "Pontus",
+                        LastName = "Fröjd",
+                        PhoneNumber = "0737058360"
+                    },
+
+                    new Track
+                    {
+                        Price = 5,
+                        Age = 25,
+                        FirstName = "Pontus",
+                        LastName = "Fröjd",
+                        PhoneNumber = "0737058360"
+                    },
+
+                    new Track
+                    {
+                        Price = 5,
+                        Age = 25,
+                        FirstName = "Pontus",
+                        LastName = "Fröjd",
+                        PhoneNumber = "0737058360"
+                    },
+
+                    new Track
+                    {
+                        Price = 5,
+                        Age = 25,
+                        FirstName = "Pontus",
+                        LastName = "Fröjd",
+                        PhoneNumber = "0737058360"
+                    }
+                ) ;
+                context.SaveChanges();
+            }
+        }
+    }
+}
