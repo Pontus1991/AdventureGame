521585f6fba08ae59b625e66d4df58da02403e56
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-11-11 22:01:41 +0100

    Lagt till nya rubriker samt ändra färg på texten

--- WebsiteSlutuppgift/WebsiteSlutuppgift/Models/Booking.cs
+++ WebsiteSlutuppgift/WebsiteSlutuppgift/Models/Booking.cs
@@ -8,10 +8,10 @@ namespace WebsiteSlutuppgift.Models
     public class Booking
     {
         public Guid Id { get; set; }
+        public Guid TrackId { get; set; }
         public string TrackName { get; set; }
         public string Booker { get; set; }
         public DateTime From { get; set; } // DateTime gör att vi får upp den där datumrutan i browsern. 
         public DateTime To { get; set; }
-        public Guid TrackId { get; internal set; }
     }
 }
