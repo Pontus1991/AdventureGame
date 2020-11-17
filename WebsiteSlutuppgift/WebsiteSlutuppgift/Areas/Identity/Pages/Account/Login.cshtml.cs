b0595355927ffb9b8eb3e212e80b6af77b9172bb
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-11-17 19:49:25 +0100

    tja

--- WebsiteSlutuppgift/WebsiteSlutuppgift/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebsiteSlutuppgift/WebsiteSlutuppgift/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -69,7 +69,6 @@ namespace WebsiteSlutuppgift.Areas.Identity.Pages.Account
 
             returnUrl = returnUrl ?? Url.Content("~/");
 
-            // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
@@ -83,8 +82,6 @@ namespace WebsiteSlutuppgift.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
@@ -107,7 +104,6 @@ namespace WebsiteSlutuppgift.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
