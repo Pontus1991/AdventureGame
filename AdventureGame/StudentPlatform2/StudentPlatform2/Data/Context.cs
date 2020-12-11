7352334ae6818da89a9789f36a4e1ac6090207e9
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-12-11 13:34:15 +0100

    Nytt Projektarbete

---
+++ AdventureGame/StudentPlatform2/StudentPlatform2/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPlatform2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentPlatform2.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { 
+        
+        }
+        public DbSet<Student> Students { get; set; } // en tabell som vi kan göra queries som vi kan stoppa in data i. 
+    }
+}
