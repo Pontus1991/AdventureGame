4d67e89a055f32637fa91f488d203a7576343589
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-12-12 16:41:08 +0100

    Ändrade startupen och la till json-info

--- AdventureGame/OnlineBibliotek/OnlineBibliotek/Startup.cs
+++ AdventureGame/OnlineBibliotek/OnlineBibliotek/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OnlineBibliotek.Data;
 
 namespace OnlineBibliotek
 {
@@ -24,7 +26,11 @@ namespace OnlineBibliotek
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+
+            services.AddDbContext<Context>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers()
+            .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
