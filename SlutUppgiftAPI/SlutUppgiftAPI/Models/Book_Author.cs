5363ab041783f4f0ca1fc0b6d721518189beab7b
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-12-14 17:07:02 +0100

    Innan migrering

--- SlutUppgiftAPI/SlutUppgiftAPI/Models/Book_Author.cs
+++ SlutUppgiftAPI/SlutUppgiftAPI/Models/Book_Author.cs
@@ -8,9 +8,8 @@ namespace SlutUppgiftAPI.Models
     public class Book_Author // Kopplingstabell
     {
         public int AuthorId { get; set; }
-        public Author  MyProperty { get; set; }
+        public Author Author { get; set; }
         public int BookId { get; set; }
-
-        public List<Book_Author> Book_Authors { get; set; }
+        public Book Book { get; set; }
     }
 }
