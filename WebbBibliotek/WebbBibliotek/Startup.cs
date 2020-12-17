e6f84579e22785ff75a86eb04e405fcfe4b77d0c
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-12-17 22:09:14 +0100

    tja

--- WebbBibliotek/WebbBibliotek/Startup.cs
+++ WebbBibliotek/WebbBibliotek/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebbBibliotek.Data;
 
 namespace WebbBibliotek
 {
