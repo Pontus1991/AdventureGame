776cdedd4595f784bd8315efd87c425a7c0d178a
Pontus Fröjd <Pontus_Frojd@hotmail.com>
2020-12-31 12:00:33 +0100

    tja

--- WebbBibliotek/WebbBibliotek/Helpers/AppSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebbBibliotek.Helpers
-{
-    public class AppSettings
-    {
-        public string Secret { get; set; }
-    }
-}
--- WebbBibliotek/WebbBibliotek/Models/RevokeTokenRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebbBibliotek.Models
-{
-    public class RevokeTokenRequest
-    {
-        public string Token { get; set; }
-    }
-}
